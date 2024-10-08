bddc49ec6e66c6d0d9adc606a8b895795e3a406d
honore.charbonnier <honore.charbonnier@epita.fr>
2024-10-08 23:44:37 +0200

    changement en struct

--- neural_network/neural_network.c
+++ neural_network/neural_network.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <err.h>
+#include <string.h>
 
 //nb of inputs neurons
 #define numInputs 2 
@@ -14,8 +16,52 @@
 //nb of Training set
 #define numTrainingSets 4
 
+
+
+double init_weights() {
+  return ((double)rand()) / ((double)RAND_MAX);
+}
+
+typedef struct
+{
+  double lr; //learning rate
+  
+  double hiddenLayer[numHiddenNodes];
+  double outputLayer[numOutputs];
+
+  double hiddenLayerBias[numHiddenNodes];
+  double outputLayerBias[numOutputs];
+
+  double hiddenWeights[numInputs][numHiddenNodes];
+  double outputWeights[numHiddenNodes][numOutputs];
+}neural_network;
+
+void init_neural_network(neural_network * nn){
+  nn->lr = 0.1;
+
+  for (int i = 0; i < numHiddenNodes; i++) {
+        for (int j = 0; j < numOutputs; j++) {
+            nn->outputWeights[i][j] = init_weights();
+        }
+    }
+    for (int i = 0; i < numHiddenNodes; i++) {
+        nn->hiddenLayerBias[i] = init_weights();
+    }
+    for (int i = 0; i < numOutputs; i++) {
+        nn->outputLayerBias[i] = init_weights();
+    }
+    for (int i = 0; i < numInputs; i++) {
+        for (int j = 0; j < numHiddenNodes; j++) {
+            nn->hiddenWeights[i][j] = init_weights();
+        }
+    }
+
+}
+
+
 //macro for printing neurons result
 #define PR(a1,a2,a3,a4) printf("In: %g %g Out: %g Predict: %g\n",a1,a2,a3,a4);
+
 double sigmoid(double x){
   return 1/ (1 + exp(-x));
 }
@@ -24,9 +70,7 @@ double dSigmoid(double x){
   return x * (1-x);
 }
 
-double init_weights() {
-  return ((double)rand()) / ((double)RAND_MAX);
-}
+
 
 void shuffle(int *array, size_t n){
   if(n>1){
@@ -40,11 +84,7 @@ void shuffle(int *array, size_t n){
 }
 
 // Fonction pour sauvegarder les poids et biais
-void save_weights_and_biases(const char* filename, 
-                              double hiddenWeights[numInputs][numHiddenNodes],
-                              double outputWeights[numHiddenNodes][numOutputs],
-                              double hiddenLayerBias[numHiddenNodes],
-                              double outputLayerBias[numOutputs]) {
+void save_weights_and_biases(const char* filename, neural_network *nn) {
     FILE *file = fopen(filename, "wb");
     if (file == NULL) {
         perror("Unable to open file for saving");
@@ -52,23 +92,19 @@ void save_weights_and_biases(const char* filename,
     }
     
     // Sauvegarde des poids cachés
-    fwrite(hiddenWeights, sizeof(double), numInputs * numHiddenNodes, file);
+    fwrite(nn->hiddenWeights, sizeof(double), numInputs * numHiddenNodes, file);
     // Sauvegarde des poids de sortie
-    fwrite(outputWeights, sizeof(double), numHiddenNodes * numOutputs, file);
+    fwrite(nn->outputWeights, sizeof(double), numHiddenNodes * numOutputs, file);
     // Sauvegarde des biais cachés
-    fwrite(hiddenLayerBias, sizeof(double), numHiddenNodes, file);
+    fwrite(nn->hiddenLayerBias, sizeof(double), numHiddenNodes, file);
     // Sauvegarde des biais de sortie
-    fwrite(outputLayerBias, sizeof(double), numOutputs, file);
+    fwrite(nn->outputLayerBias, sizeof(double), numOutputs, file);
     
     fclose(file);
 }
 
 // Fonction pour charger les poids et biais
-void load_weights_and_biases(const char* filename, 
-                             double hiddenWeights[numInputs][numHiddenNodes],
-                             double outputWeights[numHiddenNodes][numOutputs],
-                             double hiddenLayerBias[numHiddenNodes],
-                             double outputLayerBias[numOutputs]) {
+void load_weights_and_biases(const char* filename, neural_network *nn) {
     FILE *file = fopen(filename, "rb");
     if (file == NULL) {
         perror("Unable to open file for loading");
@@ -76,28 +112,28 @@ void load_weights_and_biases(const char* filename,
     }
     
     // Chargement des poids cachés
-    if (fread(hiddenWeights, sizeof(double), numInputs * numHiddenNodes, file) != numInputs * numHiddenNodes) {
+    if (fread(nn->hiddenWeights, sizeof(double), numInputs * numHiddenNodes, file) != numInputs * numHiddenNodes) {
         perror("Error reading hiddenWeights");
         fclose(file);
         return;
     }
     
     // Chargement des poids de sortie
-    if (fread(outputWeights, sizeof(double), numHiddenNodes * numOutputs, file) != numHiddenNodes * numOutputs) {
+    if (fread(nn->outputWeights, sizeof(double), numHiddenNodes * numOutputs, file) != numHiddenNodes * numOutputs) {
         perror("Error reading outputWeights");
         fclose(file);
         return;
     }
     
     // Chargement des biais cachés
-    if (fread(hiddenLayerBias, sizeof(double), numHiddenNodes, file) != numHiddenNodes) {
+    if (fread(nn->hiddenLayerBias, sizeof(double), numHiddenNodes, file) != numHiddenNodes) {
         perror("Error reading hiddenLayerBias");
         fclose(file);
         return;
     }
     
     // Chargement des biais de sortie
-    if (fread(outputLayerBias, sizeof(double), numOutputs, file) != numOutputs) {
+    if (fread(nn->outputLayerBias, sizeof(double), numOutputs, file) != numOutputs) {
         perror("Error reading outputLayerBias");
         fclose(file);
         return;
@@ -106,18 +142,41 @@ void load_weights_and_biases(const char* filename,
     fclose(file);
 }
 
+void print_weights_and_biases(neural_network *nn ) {
+    printf("Hidden Weights:\n");
+    for (int i = 0; i < numInputs; i++) {
+        for (int j = 0; j < numHiddenNodes; j++) {
+            printf("%f ", nn->hiddenWeights[i][j]);
+        }
+        printf("\n");
+    }
+    
+    printf("\nOutput Weights:\n");
+    for (int i = 0; i < numHiddenNodes; i++) {
+        for (int j = 0; j < numOutputs; j++) {
+            printf("%f ", nn->outputWeights[i][j]);
+        }
+        printf("\n");
+    }
+    
+    printf("\nHidden Layer Biases:\n");
+    for (int i = 0; i < numHiddenNodes; i++) {
+        printf("%f ", nn->hiddenLayerBias[i]);
+    }
+    printf("\n");
+    
+    printf("\nOutput Layer Biases:\n");
+    for (int i = 0; i < numOutputs; i++) {
+        printf("%f ", nn->outputLayerBias[i]);
+    }
+    printf("\n");
+}
 
-int main(void){
-  const double lr = 0.1f; //learning rate
-  
-  double hiddenLayer[numHiddenNodes];
-  double outputLayer[numOutputs];
 
-  double hiddenLayerBias[numHiddenNodes];
-  double outputLayerBias[numOutputs];
 
-  double hiddenWeights[numInputs][numHiddenNodes];
-  double outputWeights[numHiddenNodes][numOutputs];
+void train(neural_network * nn){
+
+
 
 
   double training_inputs[numTrainingSets][numInputs] = {{0.0f,0.0f},
@@ -131,17 +190,8 @@ int main(void){
                                                           {0.0f},
                                                           {1.0f}};
 
-  for(int i = 0; i< numHiddenNodes; i++){
-    for(int j = 0 ; j<numOutputs; j++){
-      outputWeights[i][j] = init_weights();
-    }
-  }
   
-  for(int j = 0; j<numOutputs; j++){
-    outputLayerBias[j] = init_weights();
-  }
-
-  load_weights_and_biases("weights_and_biases.bin", hiddenWeights, outputWeights, hiddenLayerBias, outputLayerBias);
+  load_weights_and_biases("weights_and_biases.bin", nn);
 
   int trainingSetOrder[] = {0,1,2,3};
 
@@ -160,27 +210,28 @@ int main(void){
 
       //Compute hidden layer activation
       for(int j = 0; j< numHiddenNodes; j++){
-        double activation = hiddenLayerBias[j];
+        double activation = nn->hiddenLayerBias[j];
 
         for(int k = 0; k < numInputs; k++){
-          activation += training_inputs[i][k] * hiddenWeights[k][j];
+          activation += training_inputs[i][k] * nn->hiddenWeights[k][j];
         }
 
-        hiddenLayer[j] = sigmoid(activation);
+        nn->hiddenLayer[j] = sigmoid(activation);
       }
 
       //Compute output layer activation
       for(int j = 0; j< numOutputs; j++){
-        double activation = outputLayerBias[j];
+        double activation = nn->outputLayerBias[j];
 
         for(int k = 0; k < numHiddenNodes; k++){
-          activation += hiddenLayer[k] * outputWeights[k][j];
+          activation += nn->hiddenLayer[k] * nn->outputWeights[k][j];
         }
 
-        outputLayer[j] = round(sigmoid(activation));
+        nn->outputLayer[j] = round(sigmoid(activation));
       }
       printf(" %d sur %ld    :" , epoch, numberOfEpochs);
-      PR(training_inputs[i][0], training_inputs[i][1],outputLayer[0],
+      
+      PR(training_inputs[i][0], training_inputs[i][1],nn->outputLayer[0],
           training_outputs[i][0]);
 
       // Backprop
@@ -190,8 +241,8 @@ int main(void){
       double deltaOutput[numOutputs];
       
       for( int j = 0; j < numOutputs; j++){
-        double error = (training_outputs[i][j] - outputLayer[j]);
-        deltaOutput[j] = error * dSigmoid(outputLayer[j]);
+        double error = (training_outputs[i][j] - nn->outputLayer[j]);
+        deltaOutput[j] = error * dSigmoid(nn->outputLayer[j]);
       }
 
       //Compute change in hidden weights
@@ -200,18 +251,18 @@ int main(void){
       for(int j = 0; j < numHiddenNodes; j++){
         double error = 0.0f;
         for(int k = 0; k<numOutputs; k++){
-          error += deltaOutput[k] * outputWeights[j][k];
+          error += deltaOutput[k] * nn->outputWeights[j][k];
         }
-        deltaHidden[j] = error * dSigmoid(hiddenLayer[j]);
+        deltaHidden[j] = error * dSigmoid(nn->hiddenLayer[j]);
       }
 
 
       // Apply change in output weights
 
       for(int j = 0; j< numOutputs; j++){
-        outputLayerBias[j] += deltaOutput[j] * lr;
+        nn->outputLayerBias[j] += deltaOutput[j] * nn->lr;
         for(int k = 0; k<numHiddenNodes; k++){
-          outputWeights[k][j] += hiddenLayer[k] * deltaOutput[j] * lr;
+          nn->outputWeights[k][j] += nn->hiddenLayer[k] * deltaOutput[j] * nn->lr;
         }
       }
 
@@ -219,16 +270,44 @@ int main(void){
       // Apply change in hidden weights
       
       for(int j = 0; j< numHiddenNodes; j++){
-        hiddenLayerBias[j] += deltaHidden[j] * lr;
+        nn->hiddenLayerBias[j] += deltaHidden[j] * nn->lr;
         for(int k = 0; k<numInputs; k++){
-          hiddenWeights[k][j] += training_inputs[i][k] * deltaHidden[j] * lr;
+          nn->hiddenWeights[k][j] += training_inputs[i][k] * deltaHidden[j] * nn->lr;
         }
       }
 
     }
   }
   
-  //save_weights_and_biases("weights_and_biases.bin", hiddenWeights, outputWeights, hiddenLayerBias, outputLayerBias);
+  save_weights_and_biases("weights_and_biases.bin", nn);
+  print_weights_and_biases(nn);
+}
 
+double predict(double a, double b){
+  if(!((a==1 || a==0) && (b==1 || b==0)))
+    errx(1,"valeur d'entré de predict pas bonne");
+  printf("fais la fonction connard\n entrée 1 : %g entrée 2 : %g\n",a , b );
+  return a;
+}
+
+
+
+int main(int argc, char **argv){
+  neural_network nn;
+  init_neural_network(&nn);
+  if(argc<2)
+    errx(1,"manque d'arguments");
+  else if(strcmp(argv[1],"-t")==0){
+    train(&nn);
+    return 0;
+  }
+  else if(strcmp(argv[1],"-p")==0){
+    if(argc<4)
+      errx(1,"pas assez d'arguments pour predict");
+    else
+      predict(strtod(argv[2],NULL),strtod(argv[3],NULL));
+  }
+  else
+    errx(1,"extension inconnu");
   return 0;
 }
