9e636120b80bebb7553b79ea93c480c3d6254ca2
honore.charbonnier <honore.charbonnier@epita.fr>
2024-10-11 01:10:09 +0200

    add optstr

--- neural_network/neural_network.c
+++ neural_network/neural_network.c
@@ -1,14 +1,17 @@
+#include<unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 #include <err.h>
+#include <getopt.h>
 #include <string.h>
+
 
 #include "neural_network.h"
 #include "train_neural_network.h"
 #include "neural_network_utils.h"
 
-#define OPTSTR "hvi:o:n:m:b:c:l:a:"
+
 
 double init_weights() {
   return ((double)rand()) / ((double)RAND_MAX);
@@ -42,7 +45,7 @@ void init_neural_network(neural_network * nn,double lr){
 
 void predict(neural_network * nn,double a, double b){
   if(!((a==1 || a==0) && (b==1 || b==0)))
-    errx(1,"valeur d'entrée de predict pas bonne");
+    errx(1,"not good value for predict");
   double inputs[2] = {a,b};
   for(size_t j = 0; j< numHiddenNodes; j++){
     double activation = nn->hiddenLayerBias[j];
@@ -67,25 +70,27 @@ void predict(neural_network * nn,double a, double b){
 }
 
 
-void print_help_page(char *name) {
-  printf("Usage: %s [options]\n"
+const char * print_help_page=
+  "Usage: [options]\n"
   "[TRAIN MODE OPTIONS]\n"
-  "   -n nb    :  number of training iterations (def : 1 000 000)\n"
-  "   -i file  :  file where is stored the neural network (def : weights_and_biaises.bin)\n"
-  "   -o file  :  file where to save the neural network (def : weights_and_biaises.bin)\n"
-  "   -l nb    :  learning rate (def : weights_and_biaises.bin)\n"
+  "   -n nb , --training-number nb  :  number of training iterations (def : 1 000 000)\n"
+  "   -i file , --input-file file   :  file where is stored the neural network (def : weights_and_biases.bin)\n"
+  "   -o file , --output-file file  :  file where to save the neural network (def : weights_and_biases.bin)\n"
+  "   -l nb , --learning-rate       :  learning rate (def : 0.1)\n"
+  "   -p , --print-training      :  print result while training\n"
   "[PREDICT MODE OPTIONS]\n"
-  "   -i file  :  file where is stored the neural network (def : weights_and_biaises.bin)\n"
-  "   -a n,n   :  learning rate (def : weights_and_biaises.bin)\n"
-  ,name);
-}
+  "   -i file , --input-file file   :  file where is stored the neural network (def : weights_and_biaises.bin)\n"
+  "   -a n1,n2 , --arg n1,n2        :  argument 1 and 2 for the predict can only be 0 and 1\n"
+  "[GENERAL OPTIONS]\n"
+  "   -m mode , --mode mode         : switch between the mode PREDICT and TRAIN (def : TRAIN)\n"
+  "   -h , --help                   : show this help page\n";
 
 
-
+/*
 int main(int argc, char **argv){
   neural_network nn;
   init_neural_network(&nn, 0.1);
-  //print_help_page("blase");
+  print_help_page("blase");
   if(argc<2)
     errx(1,"lake of arguments");
   else if(strcmp(argv[1],"-t")==0){
@@ -102,3 +107,124 @@ int main(int argc, char **argv){
     errx(1,"extension inconnu");
   return 0;
 }
+*/
+
+int main(int argc, char ** argv) {
+  int opt;
+  int option_index = 0;
+
+  // Struct for long opt
+  struct option long_options[] = {
+    {"help", no_argument, 0, 'h'},
+    {"mode", required_argument, 0, 'm'},
+    {"input-file", required_argument, 0, 'i'},
+    {"output-file", required_argument, 0, 'o'},
+    {"learning-rate", required_argument, 0, 'l'},
+    {"training-number", required_argument, 0, 'n'},
+    {"print-training", no_argument, 0, 'p'},
+    {"arg", required_argument, 0, 'a'},
+    {0, 0, 0, 0}
+  };
+
+  struct cust_options {
+    char *mode;
+    char *input_file;
+    char *output_file;
+    float learning_rate;
+    int print_training;
+    double arg1;
+    double arg2;
+    long int training_number;
+  };
+
+  struct cust_options opts = {
+    .mode = "TRAIN",
+    .input_file = "weights_and_biases.bin",
+    .output_file = "weights_and_biases.bin",
+    .learning_rate = 0.1,
+    .print_training = 0,
+    .arg1 = 0,
+    .arg2 = 0,
+    .training_number = 1000000
+
+  };
+
+  while ((opt = getopt_long(argc, argv, "hm:a:i:l:o:n:p", long_options, &option_index)) != -1) {
+    switch (opt) {
+      case 'h':
+        printf("%s",print_help_page);
+        return EXIT_SUCCESS;
+
+      case 'm':
+        opts.mode = optarg;
+        if((strcmp(opts.mode,"TRAIN")!=0) && (strcmp(opts.mode,"PREDICT")!=0))
+          errx(EXIT_FAILURE,"invalid argument for -m , -mode : refer to -h , -help");
+        break;
+
+      case 'a':
+      {
+        char *token = strtok(optarg, ",");
+        if (token != NULL) {
+          double temp = strtod(token, NULL);
+          if (temp ==0 || temp == 1) {
+            opts.arg1 = temp;
+          }
+          else
+            errx(EXIT_FAILURE,"invalid argument for -a , -arg : refer to -h , -help");
+          token = strtok(NULL, ",");
+          if (token != NULL) {
+            temp = strtod(token, NULL);
+            if (temp ==0 || temp == 1) {
+              opts.arg2 = temp;
+            }
+            else
+              errx(EXIT_FAILURE,"invalid argument for -a , -arg : refer to -h , -help");
+            opts.arg2 = atoi(token);
+          }
+          else {
+            errx(EXIT_FAILURE, "invalid argument for -a , -arg : refer to -h , -help");
+          }
+        }
+        else {
+          errx(EXIT_FAILURE, "invalid argument for -a , -arg : refer to -h , -help");
+        }
+      }
+      break;
+
+      case 'i':
+        opts.input_file = optarg;
+        break;
+
+      case 'o':
+        opts.output_file = optarg;
+        break;
+
+      case 'l':
+        opts.learning_rate = atof(optarg);
+        break;
+
+      case 'n':
+        opts.arg1 = atof(optarg);
+        break;
+
+      case 'p':
+        opts.print_training = 1;
+        break;
+      default:
+        errx(EXIT_FAILURE,"invalid format : refer to -h , -help");
+
+    }
+  }
+  neural_network nn;
+  init_neural_network(&nn,opts.learning_rate);
+  if (strcmp(opts.mode, "TRAIN") == 0) {
+    train(&nn, opts.input_file, opts.output_file, opts.training_number, opts.print_training);
+    return EXIT_SUCCESS;
+  } else if (strcmp(opts.mode, "PREDICT") == 0) {
+    predict(&nn, opts.arg1, opts.arg2);
+    return EXIT_SUCCESS;
+  } else {
+    errx(EXIT_FAILURE, "invalid mode : refer to -h , -help");
+  }
+}
+
